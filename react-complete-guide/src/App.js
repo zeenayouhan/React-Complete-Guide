371aa73b6f96c2352aadfa7c31d653b75e278669
zeenayouhan <zinayouhan33@gmail.com>
2020-04-11 12:43:04 +0530

    CompilationFormat

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi I am Zameer Younus</h1>
     </div>
+      // js compile to this one-> React.createElement('div',{className:"App"},React.createElement('h1',null,"Hi I am Zameer Younus"))
+    
   );
+
 }
 
 export default App;
