12cf6e2601488ea9c553205a9acb899c6536194e
zeenayouhan <zinayouhan33@gmail.com>
2020-04-11 22:00:11 +0530

    State

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+
+  state={
+    Person:[
+      {name:"Zeena", age:"25"},
+      {name:"Lashan", age:'26'}
+    ]
+  }
+
+
+  render() {
+    return(
     <div className="App">
       <h1>Hi I am Zameer Younus</h1>
+      <Person name1={this.state.Person[0].name} age={this.state.Person[0].age}/>
+      <Person name1={this.state.Person[1].name} age={this.state.Person[1].age}/>
+      <button>Switch</button>
     </div>
       // js compile to this one-> React.createElement('div',{className:"App"},React.createElement('h1',null,"Hi I am Zameer Younus"))
-    
-  );
+    )
+  };
 
 }
 
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = (props) =>{
+    return (
+        <div>
+            <p>I am a person named {props.name1} and {props.age}.</p>
+       
+        </div>
+    
+        )
+}
+
+export default person;
