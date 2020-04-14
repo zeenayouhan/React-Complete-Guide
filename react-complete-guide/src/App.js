d3665089f0e235d05f01ced604f0a946765d8f3c
zeenayouhan <zinayouhan33@gmail.com>
2020-04-14 21:59:44 +0530

    Hook-usestate

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,42 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-class App extends Component {
 
-  state={
-    Person:[
-      {name:"Zeena", age:"25"},
-      {name:"Lashan", age:'26'}
-    ]
-  }
 
-  switchHandler=()=>{
-    //console.log("Clicky");
-    this.setState({
-      Person:[
-        {name:"ZeenaL", age:"25"},
-        {name:"LashanZ", age:'26'}
+
+const App = props => {
+   const [personState,setPersonState]= useState({
+     Person:[
+        {name:"Zeena", age:"25"},
+        {name:"Zamemr", age:'26'}
       ]
-    }
-
-    )
-
-  }
-
-
-  render() {
-    return(
-    <div className="App">
-      <h1>Hi I am Zameer Younus</h1>
-      <Person name1={this.state.Person[0].name} age={this.state.Person[0].age}/>
-      <Person name1={this.state.Person[1].name} age={this.state.Person[1].age}/>
-      <button onClick={this.switchHandler}>Switch</button>
-    </div>
-      // js compile to this one-> React.createElement('div',{className:"App"},React.createElement('h1',null,"Hi I am Zameer Younus"))
-    )
-  };
+     });
+     const switchHandler=()=>{
+              //console.log("Clicky");
+              setPersonState({
+                Person:[
+                  {name:"ZeenaL", age:"25"},
+                  {name:"Zameer", age:'26'}
+                ]
+              });
+            }
+        return(
+        <div className="App">
+          <h1>Hi I am Zameer Younus</h1>
+          <Person name1={personState.Person[0].name} age={personState.Person[0].age}/>
+          <Person name1={personState.Person[1].name} age={personState.Person[1].age}/>
+          <button onClick={switchHandler}>Switch</button>
+        </div>
+          // js compile to this one-> React.createElement('div',{className:"App"},React.createElement('h1',null,"Hi I am Zameer Younus"))
+        )
+      
 
 }
 
 export default App;
+
+// class App extends Component {
+
+//   state={
+//     Person:[
+//       {name:"Zeena", age:"25"},
+//       {name:"Zammer", age:'26'}
+//     ]
+//   }
+
+//   switchHandler=()=>{
+//     //console.log("Clicky");
+//     this.setState({
+//       Person:[
+//         {name:"ZeenaL", age:"25"},
+//         {name:"Zammer", age:'26'}
+//       ]
+//     }
+
+//     )
+
+//   }
+
+
+//   render() {
+//     return(
+//     <div className="App">
+//       <h1>Hi I am Zameer Younus</h1>
+//       <Person name1={personState.Person[0].name} age={personState.Person[0].age}/>
+//       <Person name1={personState.Person[1].name} age={personState.Person[1].age}/>
+//       <button onClick={this.switchHandler}>Switch</button>
+//     </div>
+//       // js compile to this one-> React.createElement('div',{className:"App"},React.createElement('h1',null,"Hi I am Zameer Younus"))
+//     )
+//   };
+
+// }
+
+// export default App;
