63dd4eca34c5c84f367342007e66203620222771
zeenayouhan <zinayouhan33@gmail.com>
2020-04-15 11:04:33 +0530

    Bind function

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,76 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
 
 
 
-const App = props => {
-   const [personState,setPersonState]= useState({
-     Person:[
-        {name:"Zeena", age:"25"},
-        {name:"Zamemr", age:'26'}
-      ]
-     });
-     const switchHandler=()=>{
-              //console.log("Clicky");
-              setPersonState({
-                Person:[
-                  {name:"ZeenaL", age:"25"},
-                  {name:"Zameer", age:'26'}
-                ]
-              });
-            }
-        return(
-        <div className="App">
-          <h1>Hi I am Zameer Younus</h1>
-          <Person name1={personState.Person[0].name} age={personState.Person[0].age}/>
-          <Person name1={personState.Person[1].name} age={personState.Person[1].age}/>
-          <button onClick={switchHandler}>Switch</button>
-        </div>
-          // js compile to this one-> React.createElement('div',{className:"App"},React.createElement('h1',null,"Hi I am Zameer Younus"))
-        )
-      
-
-}
-
-export default App;
-
-// class App extends Component {
-
-//   state={
-//     Person:[
-//       {name:"Zeena", age:"25"},
-//       {name:"Zammer", age:'26'}
-//     ]
-//   }
-
-//   switchHandler=()=>{
-//     //console.log("Clicky");
-//     this.setState({
-//       Person:[
-//         {name:"ZeenaL", age:"25"},
-//         {name:"Zammer", age:'26'}
+// const App = props => {
+//    const [personState,setPersonState]= useState({
+//      Person:[
+//         {name:"Zeena", age:"25"},
+//         {name:"Zamemr", age:'26'}
 //       ]
-//     }
-
-//     )
-
-//   }
-
-
-//   render() {
-//     return(
-//     <div className="App">
-//       <h1>Hi I am Zameer Younus</h1>
-//       <Person name1={personState.Person[0].name} age={personState.Person[0].age}/>
-//       <Person name1={personState.Person[1].name} age={personState.Person[1].age}/>
-//       <button onClick={this.switchHandler}>Switch</button>
-//     </div>
-//       // js compile to this one-> React.createElement('div',{className:"App"},React.createElement('h1',null,"Hi I am Zameer Younus"))
-//     )
-//   };
+//      });
+//      const switchHandler=()=>{
+//               //console.log("Clicky");
+//               setPersonState({
+//                 Person:[
+//                   {name:"ZeenaL", age:"25"},
+//                   {name:"Zameer", age:'26'}
+//                 ]
+//               });
+//             }
+//         return(
+//         <div className="App">
+//           <h1>Hi I am Zameer Younus</h1>
+//           <Person name1={personState.Person[0].name} age={personState.Person[0].age}/>
+//           <Person name1={personState.Person[1].name} age={personState.Person[1].age}/>
+//           <button onClick={switchHandler}>Switch</button>
+//         </div>
+//           // js compile to this one-> React.createElement('div',{className:"App"},React.createElement('h1',null,"Hi I am Zameer Younus"))
+//         )
+      
 
 // }
 
 // export default App;
+
+class App extends Component {
+
+  state={
+    Person:[
+      {name:"Zeena", age:"25"},
+      {name:"Zammer", age:'26'}
+    ]
+  }
+
+  switchHandler=(newName)=>{
+    //console.log("Clicky");
+    this.setState({
+      Person:[
+        {name:newName, age:"25"},
+        {name:"Zammer", age:'26'}
+      ]
+    }
+
+    )
+
+  }
+
+
+  render() {
+    return(
+    <div className="App">
+      <h1>Hi I am Zameer Younus</h1>
+      <Person name1={this.state.Person[0].name} age={this.state.Person[0].age}/>
+      <Person name1={this.state.Person[1].name}  click={this.switchHandler.bind(this,"lZena")} age={this.state.Person[1].age}/>
+      <button onClick={()=>this.switchHandler("Zeenaaaa")}>Switch</button>
+    </div>
+      // js compile to this one-> React.createElement('div',{className:"App"},React.createElement('h1',null,"Hi I am Zameer Younus"))
+    )
+  };
+
+}
+
+export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const person = (props) =>{
     return (
-        <div>
-            <p>I am a person named {props.name1} and {props.age}.</p>
+        <div onClick={props.click}>
+            <p>I am a person  named {props.name1} and {props.age}.</p>
        
         </div>
     
